310a8e696973be74afd1999fb93b6c001e71468d
James Schumann <jschumann@gmail.com>
2017-09-01 18:50:09 +0000

    added people blob bindings

--- people/index.js
+++ people/index.js
@@ -1,4 +1,4 @@
 module.exports = function (context, peopleNow) {
-    context.log("JavaScript blob trigger function processed blob peopleNow:\n Blob Size:", peopleNow.length, "Bytes");
+    context.log("JavaScript blob trigger function processed blob peopleNow.json");
     context.done();
 };
