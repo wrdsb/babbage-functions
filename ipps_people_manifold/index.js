74c0478fe864e78b8b20c6ecc82ee19486e2adf7
James Schumann <jschumann@gmail.com>
2017-09-07 15:15:01 +0000

    stub people manifold

---
+++ ipps_people_manifold/index.js
@@ -0,0 +1,15 @@
+module.exports = function(context) {
+    context.log('Node.js queue trigger function processed work item', context.bindings.peopleDiffQueue);
+    context.log('queueTrigger =', context.bindingData.queueTrigger);
+    context.log('expirationTime =', context.bindingData.expirationTime);
+    context.log('insertionTime =', context.bindingData.insertionTime);
+    context.log('nextVisibleTime =', context.bindingData.nextVisibleTime);
+    context.log('id=', context.bindingData.id);
+    context.log('popReceipt =', context.bindingData.popReceipt);
+    context.log('dequeueCount =', context.bindingData.dequeueCount);
+    context.done();
+
+    function process_change(ein, change) {
+        
+    }
+};
