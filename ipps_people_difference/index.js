b3372371ce92b9e1d2205ac0ce2ba7ac855e55a2
James Schumann <jschumann@gmail.com>
2017-09-07 13:13:47 +0000

    issue with null end dates?

--- ipps_people_difference/index.js
+++ ipps_people_difference/index.js
@@ -193,8 +193,8 @@ module.exports = function (context) {
                     (old_position.ipps_extension                  != new_position.ipps_extension) ||
                     (old_position.ipps_home_location_indicator    != new_position.ipps_home_location_indicator) ||
                     (old_position.ipps_activity_code              != new_position.ipps_activity_code) ||
-                    (old_position.ipps_position_start_date        != new_position.ipps_position_start_date) ||
-                    (old_position.ipps_position_end_date          != new_position.ipps_position_end_date)
+                    (old_position.ipps_position_start_date        != new_position.ipps_position_start_date) // ||
+                    //(old_position.ipps_position_end_date          != new_position.ipps_position_end_date)
                 ) {
                     updated_positions[new_position_id] = {old_position: old_position, new_position: new_position};
                 }
