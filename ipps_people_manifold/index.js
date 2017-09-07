a6f1ed6fc165d34cf2e7b22787028142eebde05d
James Schumann <jschumann@gmail.com>
2017-09-07 15:28:06 +0000

    connect manifold to queue trigger

--- ipps_people_manifold/index.js
+++ ipps_people_manifold/index.js
@@ -1,4 +1,8 @@
 module.exports = function(context) {
+    var ein = context.bindings.peopleDiffQueue.ein;
+    var change = context.bindings.peopleDiffQueue.change;
+    process_change(ein, change);
+
     context.log('Node.js queue trigger function processed work item', context.bindings.peopleDiffQueue);
     context.log('queueTrigger =', context.bindingData.queueTrigger);
     context.log('expirationTime =', context.bindingData.expirationTime);
@@ -10,6 +14,14 @@ module.exports = function(context) {
     context.done();
 
     function process_change(ein, change) {
-        
+        if (change.delete) {
+            context.log('Found delete for ' + ein);
+        } else if (change.create) {
+            context.log('Found create for ' + ein);
+        } else if (change.update) {
+            context.log('Found update for ' + ein);
+        } else {
+            context.log('Unknown change type for ' + ein);
+        }
     }
 };
