567017f5054909d164c8dd508c4929f4f2b56e61
James Schumann <jschumann@gmail.com>
2017-09-12 17:26:06 +0000

    stub group_memberships_difference

---
+++ group_memberships_difference/index.js
@@ -0,0 +1,30 @@
+module.exports = function (context) {
+    // give our bindings more human-readable names
+    var memberships_ideal = context.bindings.membershipsIdeal;
+    var memberships_actual = context.bindings.membershipsActual;
+    var memberships_diff = context.bindings.membershipsDiff;
+
+    // objects to store our diff parts
+    var missing_memberships = {};
+    var extra_memberships = {};
+
+    Object.getOwnPropertyNames(memberships_ideal).forEach(function (member) {
+        if (!memberships_actual[member]) {
+            console.log('Did not find member: ' + member);
+            missing_memberships[member] = memberships_ideal[member];
+        }
+    });
+
+    Object.getOwnPropertyNames(memberships_actual).forEach(function (member) {
+        if (!memberships_ideal[member]) {
+            console.log('Found extra member: ' + member);
+            extra_memberships[member] = memberships_actual[member];
+        }
+    });
+
+    memberships_diff = {
+        missing_memberships: {missing_memberships},
+        extra_memberships: {extra_memberships}
+    };
+    context.done();
+};
