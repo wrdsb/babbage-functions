89bd6d959fcc710da14414d65eb25e39052715ed
James Schumann <jschumann@gmail.com>
2017-09-12 18:02:37 +0000

    log name of group being processed

--- group_memberships_difference/index.js
+++ group_memberships_difference/index.js
@@ -1,4 +1,4 @@
-module.exports = function (context) {
+module.exports = function (context, info) {
     // give our bindings more human-readable names
     var memberships_ideal = context.bindings.membershipsIdeal;
     var memberships_actual = context.bindings.membershipsActual;
@@ -8,10 +8,16 @@ module.exports = function (context) {
     var missing_memberships = {};
     var extra_memberships = {};
 
+    if ('filename' in info) {
+        context.log('Processing data for ' + info.filename);
+    }
+
     Object.getOwnPropertyNames(memberships_ideal).forEach(function (member) {
         if (!memberships_actual[member]) {
             console.log('Did not find member: ' + member);
             missing_memberships[member] = memberships_ideal[member];
+        } else {
+            
         }
     });
 
