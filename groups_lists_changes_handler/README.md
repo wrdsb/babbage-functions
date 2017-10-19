83733390f2a0f8f30e26fae2343390b35804ce2c
James Schumann <jschumann@gmail.com>
2017-10-19 12:00:49 +0000

    remove refresh of previous file from groups_lists_changes_handler

--- groups_lists_changes_handler/README.md
+++ groups_lists_changes_handler/README.md
@@ -1,6 +1,5 @@
 # Groups Lists Changes Handler
 Watches for changes in lists of groups and then handles those changes.
 
-* Overwrites 'previous' file with 'now' file to prepare for the next diff calculation
 * Manufactures events by publishing individual changes or change sets to ServiceBus topic
 * Logs changes to Skyline
--- groups_lists_changes_handler/index.js
+++ groups_lists_changes_handler/index.js
@@ -2,10 +2,6 @@ module.exports = function (context) {
     var filename = context.bindingData.filename;
     var group_name = filename.replace('.json', '');
 
-    // Overwrite previous file with now file
-    context.log('Overwrite previous file with contents now file for ' + filename);
-    context.bindings.groupsListPrevious = context.bindings.groupsListNow;
-
     // Manufacture events by publishing individual changes or change sets to ServiceBus topic
     context.bindings.groupsListDiffTopic = context.bindings.groupsListDiff;
 
