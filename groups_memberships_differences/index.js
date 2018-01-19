a7907667fa7365cb06f365c17ec504e5c98b32ab
James Schumann <jschumann@gmail.com>
2018-01-19 19:37:46 +0000

    tweak output of groups_memberships_differences

--- groups_memberships_differences/index.js
+++ groups_memberships_differences/index.js
@@ -19,11 +19,15 @@ module.exports = function (context, data) {
     Object.getOwnPropertyNames(memberships_ideal).forEach(function (member) {
         if (!memberships_actual[member]) {
             console.log('Did not find member: ' + member);
-            missing_memberships.push = memberships_ideal[member];
+            var missing_member = memberships_ideal[member];
+            missing_member.groupKey = group;
+            missing_memberships.push(missing_member);
         } else {
             if (memberships_actual[member].role != memberships_ideal[member].role) {
                 context.log(memberships_actual[member].email +' role changed from '+ memberships_actual[member].role +' to '+ memberships_ideal[member].role +' in '+ group);
-                changed_memberships.push = memberships_ideal[member];
+                var changed_member = memberships_ideal[member];
+                changed_member.groupKey = group;
+                changed_memberships.push(changed_member);
             }
         }
     });
@@ -31,7 +35,10 @@ module.exports = function (context, data) {
     Object.getOwnPropertyNames(memberships_actual).forEach(function (member) {
         if (!memberships_ideal[member]) {
             console.log('Found extra member: ' + member);
-            extra_memberships.push = memberships_actual[member];
+            var extra_member = {};
+            extra_member.email = memberships_actual[member].email;
+            extra_member.groupKey = group;
+            extra_memberships.push(extra_member);
         }
     });
 
