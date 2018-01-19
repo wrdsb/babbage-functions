ac4ced49641e5f9e3fc19efc9c99ad244113a282
James Schumann <jschumann@gmail.com>
2018-01-19 18:54:07 +0000

    change missing, changed, extra memberships lists to arrays

--- groups_memberships_differences/index.js
+++ groups_memberships_differences/index.js
@@ -9,9 +9,9 @@ module.exports = function (context, data) {
     var memberships_ideal = Object.assign(memberships_ipps, memberships_supplemental, memberships_central);
 
     // objects to store our diff parts
-    var missing_memberships = {};
-    var changed_memberships = {};
-    var extra_memberships = {};
+    var missing_memberships = [];
+    var changed_memberships = [];
+    var extra_memberships = [];
     var diff = {};
 
     context.log('Processing data for ' + group);
@@ -19,11 +19,11 @@ module.exports = function (context, data) {
     Object.getOwnPropertyNames(memberships_ideal).forEach(function (member) {
         if (!memberships_actual[member]) {
             console.log('Did not find member: ' + member);
-            missing_memberships[member] = memberships_ideal[member];
+            missing_memberships.push = memberships_ideal[member];
         } else {
             if (memberships_actual[member].role != memberships_ideal[member].role) {
                 context.log(memberships_actual[member].email +' role changed from '+ memberships_actual[member].role +' to '+ memberships_ideal[member].role +' in '+ group);
-                changed_memberships[member] = memberships_ideal[member];
+                changed_memberships.push = memberships_ideal[member];
             }
         }
     });
@@ -31,7 +31,7 @@ module.exports = function (context, data) {
     Object.getOwnPropertyNames(memberships_actual).forEach(function (member) {
         if (!memberships_ideal[member]) {
             console.log('Found extra member: ' + member);
-            extra_memberships[member] = memberships_actual[member];
+            extra_memberships.push = memberships_actual[member];
         }
     });
 
