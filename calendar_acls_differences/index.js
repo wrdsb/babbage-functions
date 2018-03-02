8f8cac0cd7309841e649d2187905c78f0fc52bcd
James Schumann <jschumann@gmail.com>
2018-03-02 14:23:28 +0000

    align code and data structures to match group_memberships_differences

--- calendar_acls_differences/index.js
+++ calendar_acls_differences/index.js
@@ -1,32 +1,31 @@
 module.exports = function (context, data) {
     var calendar_id = data.calendar;
 
-    // give our bindings more human-readable names
-    var memberships_actual =  context.bindings.aclsActual;
-    var memberships_central = context.bindings.aclsCentral;
-    var memberships_ipps =    context.bindings.aclsIPPS;
+    var memberships_actual = context.bindings.membershipsActual;
+    var memberships_ipps = context.bindings.membershipsIPPS;
+    var memberships_central = data.memberships.central;
+    var memberships_supplemental = data.memberships.supplemental;
 
-    var memberships_ideal = Object.assign(memberships_ipps, memberships_central);
+    var memberships_ideal = Object.assign(memberships_ipps, memberships_supplemental, memberships_central);
 
     // objects to store our diff parts
-    var ideal_minus_actual = {};
-    var actual_minus_ideal = {};
-    var incongruent_memberships = {};
+    var missing_memberships = [];
+    var changed_memberships = [];
+    var extra_memberships = [];
     var diff = {};
 
-    context.log('Processing data for ' + calendar_id);
+    context.log('Calculating membership diff for Calendar ' + calendar_id);
 
     Object.getOwnPropertyNames(memberships_ideal).forEach(function (member) {
         if (!memberships_actual[member]) {
             console.log('Did not find member: ' + member);
-            ideal_minus_actual[member] = memberships_ideal[member];
+            var missing_member = memberships_ideal[member];
+            missing_memberships.push(missing_member);
         } else {
             if (memberships_actual[member].role != memberships_ideal[member].role) {
-                context.log(memberships_actual[member].email +' role changed from '+ memberships_actual[member].role +' to '+ memberships_ideal[member].role +' in '+ calendar_id);
-                incongruent_memberships[member] = {
-                    ideal: memberships_ideal[member],
-                    actual: memberships_actual[member]
-                };
+                context.log(memberships_actual[member].scope.value +' role changed from '+ memberships_actual[member].role +' to '+ memberships_ideal[member].role +' in '+ calendar_id);
+                var changed_member = memberships_ideal[member];
+                changed_memberships.push(changed_member);
             }
         }
     });
@@ -34,33 +33,23 @@ module.exports = function (context, data) {
     Object.getOwnPropertyNames(memberships_actual).forEach(function (member) {
         if (!memberships_ideal[member]) {
             console.log('Found extra member: ' + member);
-            actual_minus_ideal[member] = memberships_actual[member];
+            var extra_member = {};
+            extra_member.rule_id = memberships_actual[member].id;
+            extra_member.calendar_id = calendar_id;
+            extra_memberships.push(extra_member);
         }
     });
 
-    if (Object.getOwnPropertyNames(ideal_minus_actual).length > 0) {
-        diff.ideal_minus_actual = ideal_minus_actual;
-    } else {
-        diff.ideal_minus_actual = null;
-    }
-
-    if (Object.getOwnPropertyNames(actual_minus_ideal).length > 0) {
-        diff.actual_minus_ideal = actual_minus_ideal;
-    } else {
-        diff.actual_minus_ideal = null;
-    }
-
-    if (Object.getOwnPropertyNames(incongruent_memberships).length > 0) {
-        diff.incongruent_memberships = incongruent_memberships;
-    } else {
-        diff.incongruent_memberships = null;
-    }
+    diff.missing_memberships = missing_memberships;
+    diff.changed_memberships = changed_memberships;
+    diff.extra_memberships = extra_memberships;
 
     context.log(diff);
-    context.bindings.aclsDiff = diff;
+    context.bindings.membershipsDiff = diff;
     context.res = {
         status: 200,
         body: JSON.stringify(diff)
     };
-    context.done();
+
+    context.done(null, 'Finished calculating membership diff for Calendar ' + calendar_id);
 };
