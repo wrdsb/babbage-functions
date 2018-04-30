47c859a9c43e7826f96c917abd6619377a56e0dc
James Schumann <jschumann@gmail.com>
2018-04-30 13:59:21 -0400

    fix copy_blob_from_flenderson

--- copy_blob_from_flenderson/README.md
+++ copy_blob_from_flenderson/README.md
@@ -1,2 +1,16 @@
 # Copy Blob from Flenderson
 Copy a blob from Flenderson's storage account into Babbage's storage account.
+
+Request JSON: 
+```json
+{
+    "in_blob": {
+        "path":"ipps",
+        "name":"people-now-object.json"
+    },
+    "out_blob": {
+        "path":"ipps",
+        "name":"people-previous.json"
+    }
+}
+```
--- copy_blob_from_flenderson/index.js
+++ copy_blob_from_flenderson/index.js
@@ -1,6 +1,15 @@
 module.exports = function (context, data) {
-    context.log(context.bindings);
-
     context.bindings.outBlob = context.bindings.inBlob;
+
+    var event = "Replaced contents of ";
+    event += "Babbage Storage: " + data.out_blob.path + "/" + data.out_blob.name;
+    event += " with ";
+    event += "Flenderson Storage: " + data.in_blob.path + "/" + data.in_blob.name;
+
+    context.log(event);
+    context.res = {
+        status: 200,
+        body: event
+    };
     context.done(null, "Blob contents replaced.");
 };
