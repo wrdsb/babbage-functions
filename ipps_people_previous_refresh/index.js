3226db6f96ea06827bd0d69cf4df4715d6e0d228
James Schumann <jschumann@gmail.com>
2018-04-30 10:40:23 -0400

    remove disabled function ipps_people_previous_refresh

--- ipps_people_previous_refresh/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(context) {
-    context.log('Overwrite people_previous.json with contents of people_now.json');
-    context.bindings.peoplePrevious = context.bindings.peopleNow;
-    context.done();
-};
